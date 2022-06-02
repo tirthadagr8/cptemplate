65ed5b19cf44a4568ac868dd471fd636bd5cdbc9
Tirthankar Ghosh <55987952+tirthadagr8@users.noreply.github.com>
2022-06-02 12:51:06 +0530

    Create cptemplate.cpp

---
+++ cptemplate.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi M_PI
+#define Size_N  int(1e5+10)
+using namespace std;
+
+/* -- graph -- */
+bool vis[Size_N];
+vector<ll> graph[Size_N];
+
+//void graphinput()
+
+void dfs(/* jo pass karna hai*/)
+{
+    /* 1 way to go each cell */
+    // vis[vertex]=true;
+    // for(int child:g[vertex])
+    // {
+    //     if(vis[child])
+    //         continue;
+    //     dfs(child);
+    // }
+    /*2 way to go each cell*/
+    // if(i>=0 && j>=0 and i<m and j<n and grid[i][j] =='1')
+    //     {
+    //         grid[i][j] = '0';
+    //         dfs(i + 1, j, grid);
+    //         dfs(i - 1, j, grid);
+    //         dfs(i, j - 1, grid);
+    //         dfs(i, j + 1, grid);
+    //     }
+}
+
+void solve()
+{
+    // temp
+}
+
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
