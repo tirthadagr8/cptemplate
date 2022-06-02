e2414c3688819813fcaf64edfc61e0d6137a0432
Tirthankar Ghosh <55987952+tirthadagr8@users.noreply.github.com>
2022-06-02 12:51:41 +0530

    Update cptemplate.cpp

--- cptemplate.cpp
+++ cptemplate.cpp
@@ -2,18 +2,20 @@
 #define ll long long int
 #define ld long double
 #define pi M_PI
-#define Size_N  int(1e5+10)
+#define Size_N  int(1e3+10)
 using namespace std;
 
 /* -- graph -- */
-bool vis[Size_N];
-vector<ll> graph[Size_N];
-
+bool vis_1d;
+bool vis_2d[Size_N][Size_N];
+vector<ll> graph_1d[Size_N];
+vector<vector<ll>> graph_2d[Size_N][Size_N];
 //void graphinput()
 
-void dfs(/* jo pass karna hai*/)
+void dfs(ll i,ll j,vector<vector<ll>> grid)
 {
     /* 1 way to go each cell */
+    
     // vis[vertex]=true;
     // for(int child:g[vertex])
     // {
@@ -21,15 +23,22 @@ void dfs(/* jo pass karna hai*/)
     //         continue;
     //     dfs(child);
     // }
+    
     /*2 way to go each cell*/
-    // if(i>=0 && j>=0 and i<m and j<n and grid[i][j] =='1')
-    //     {
-    //         grid[i][j] = '0';
-    //         dfs(i + 1, j, grid);
-    //         dfs(i - 1, j, grid);
-    //         dfs(i, j - 1, grid);
-    //         dfs(i, j + 1, grid);
-    //     }
+    
+    ll m=grid.size();
+    ll n=grid[0].size();
+    if(vis_2d[i][j])
+        return;
+    if(i>=0 && j>=0 and i<m and j<n)
+        {
+            //grid[i][j] = 0;
+            vis_2d[i][j]=true;
+            dfs(i + 1, j, grid);
+            dfs(i - 1, j, grid);
+            dfs(i, j - 1, grid);
+            dfs(i, j + 1, grid);
+        }
 }
 
 void solve()
@@ -39,7 +48,7 @@ void solve()
 
 int main()
 {
-    int t;cin>>t;
+    ll t;cin>>t;
     while(t--)
     {
         
