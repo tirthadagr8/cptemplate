2512b5c7529f015a851cdbf69730bb61a377fb30
Tirthankar Ghosh <55987952+tirthadagr8@users.noreply.github.com>
2022-06-02 13:20:58 +0530

    Update cptemplate.cpp

--- cptemplate.cpp
+++ cptemplate.cpp
@@ -3,6 +3,8 @@
 #define ld long double
 #define pi M_PI
 #define Size_N  int(1e3+10)
+#define loop(i,s,e) for(int i=s;i<e;i++)
+#define loopr(i,s,e) for(int i=s;i<e;i--)
 using namespace std;
 
 /* -- graph -- */
@@ -11,7 +13,21 @@ bool vis_2d[Size_N][Size_N];
 //vector<ll> graph_1d[Size_N];
 //vector<vector<ll>> graph_2d[Size_N][Size_N];
 //void graphinput()
-
+ll vecsum(vector<vector<ll>> vec)
+{
+    ll sum=0;
+    loop(i,0,vec.size())
+    loop(j,0,vec[0].size())
+    sum+=vec[i][j];
+    return sum;
+}
+ll vecsum(vector<ll> vec)
+{
+    ll sum=0;
+    loop(i,0,vec.size())
+    sum+=vec[i];
+    return sum;
+}
 void dfs(ll i,ll j,vector<vector<ll>> grid)
 {
     /* 1 way to go each cell */
